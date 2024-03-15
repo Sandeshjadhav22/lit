5e88bf0746bc3c3a31d345c669b65c7bf5f12f0d
Sandesh <sandeshjadhav2002@gmail.com>
2024-03-15 12:22:42 +0530

    feat(css): changed design

--- index.js
+++ index.js
@@ -17,18 +17,25 @@ app.get("/", (req, res) => {
 app.get("/location", (req, res) => {
   res.render("Getlocation")
 })
-app.get("/:location/:group", (req, res) => {
-  res.render("GetGroup")
-})
+
 
 app.post("/location", (req, res) => {
   const location = req.body.location
   // const groups = Groups.find({"pincode":`${location}`})
 
-  const groups = [{ name: "sec 19" }, { name: "masjid" }]
-  res.render("Groups", { groups })
-})
+  const groups = [{ name: "sec 19",id:1 }, { name: "masjid",id:2 }]
+  res.render("Groups",{groups : groups});
 
+})
+app.get("/:groupid",(req,res)=>{
+  const gId = req.params.groupid
+  //fetch images from group
+  // const data = Group.find({id:`${gId}`}) 
+  // const images = data.images
+ const images = [{url:"fjfkgfjj"},{url:"gdjhy"}]
+ res.render("Group",{images}) 
+
+})
 // server listening
 app.listen(PORT, () => {
   console.log(`The app start on http://localhost:${PORT}`);
